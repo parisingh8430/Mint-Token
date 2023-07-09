7995c8a7374a32269bd9deca969c0ca9d3c71c06
CEDoromal <30488557+CEDoromal@users.noreply.github.com>
2023-07-09 17:46:57 +0800

    ERC20 Token Implementation

---
+++ contracts/MyToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+
+    address owner;
+    
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
+        _mint(msg.sender, 100 * 10**uint(decimals()));
+    }
+
+    function mint(address account, uint256 amount) public {
+        require(msg.sender == owner, "Only the owner of this contract can mint its tokens");
+        _mint(account, amount);
+    }
+    
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+    
+}
